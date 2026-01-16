20faba2828d219dad829930b9c499b2cd83b1554
sudhir kumar <sudhirkumar@sudhirs-MacBook-Air.local>
2026-01-16 17:05:12 +0530

    variable and data type

--- class1/index.js
+++ class1/index.js
@@ -2,5 +2,65 @@
 
 // variable
 
-var name = "my name is sudhir";
-console.log(name);
+// var name = "my name is sudhir";
+// console.log(name);
+
+
+// Datatype
+
+// Primitive data type
+
+
+/**
+ * String
+ * Number
+ * Boolean
+ * Null
+ * Undefined
+ * BigInt
+ * Symbol
+ */
+
+
+// string data type
+
+// let name = "sudhir kumar";
+// console.log(typeof(name));
+
+
+// Number
+// let marks = 260;
+// console.log(typeof(marks));
+
+
+// Boolean
+
+// let result = true;
+// console.log(typeof(result));
+
+// let subject = null;
+// console.log(typeof(subject));
+
+// undefined
+
+// let title;
+// console.log(typeof(title));
+
+// bigInt
+
+// let number = 3473287483247328743287423n;
+// console.log(typeof(number));
+
+// symbal // use to create unique property in some where
+
+let symb = Symbol("unique");
+console.log(typeof(symb));
+
+
+// Non premitive datatype or Refrence datatype
+
+/**
+ * Object
+ * arra
+ * function
+ */
