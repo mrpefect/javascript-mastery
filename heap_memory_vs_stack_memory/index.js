746edee5616c7444c5b2aee1fe4e48fca7d4afc5
sudhir kumar <sudhirkumar@sudhirs-MacBook-Air.local>
2026-01-17 14:01:58 +0530

    datatype coversion

--- heap_memory_vs_stack_memory/index.js
+++ heap_memory_vs_stack_memory/index.js
@@ -19,8 +19,8 @@
 
 // Stack memory
 
-let num1 = 5;
-let num2 = num1;
-num2 = 20;
-console.log(num1);
-console.log(num2);
+// let num1 = 5;
+// let num2 = num1;
+// num2 = 20;
+// console.log(num1);
+// console.log(num2);
