a2209dbddbed009b66d312cb2ae3059da8c50cef
sudhir kumar <sudhirkumar@sudhirs-MacBook-Air.local>
2026-01-17 12:32:26 +0530

    heap and stack memory

---
+++ heap_memory_vs_stack_memory/index.js
@@ -0,0 +1,26 @@
+// Heap VS Stack Memory
+
+// Heap : heap memory is use for dynamic memory allocation, while stack is used for constant memory 
+
+
+// let person1 = {
+//     name : "javis",
+//     age : 20,
+// };
+
+
+// let person2 = person1;
+// person2.name = "sudhir";
+
+// console.log(person1);
+// console.log(person2);
+
+
+
+// Stack memory
+
+let num1 = 5;
+let num2 = num1;
+num2 = 20;
+console.log(num1);
+console.log(num2);
--- non-primitive-data-type/index.js
+++ non-primitive-data-type/index.js
@@ -37,7 +37,7 @@
 // greet();
 
 
-alert("hey");
+// alert("hey");
 
 
 
