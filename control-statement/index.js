166d7e7c30825e41260d35a61ad0eda46b4dc23d
sudhir kumar <sudhirkumar@sudhirs-MacBook-Air.local>
2026-01-18 19:37:23 +0530

    conditional-statemet

---
+++ control-statement/index.js
@@ -0,0 +1,15 @@
+// if-else
+// switch-case
+// Ternary operator
+
+
+
+// if-else
+// let num = 20;
+
+// if(num <= 20){
+//     console.log('You are below 20');
+// }else{
+//     console.log('You are above 20');
+// }
+
