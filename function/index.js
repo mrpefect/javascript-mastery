4a6df3dcfee544b858a6c76693c11ba96b82666e
sudhir kumar <sudhirkumar@sudhirs-MacBook-Air.local>
2026-01-19 21:57:04 +0530

    function

---
+++ function/index.js
@@ -0,0 +1,21 @@
+// function in javascript
+
+// function greeting(){
+//     console.log("Hey i am function");
+// };
+
+// greeting();
+
+// function with expression
+
+// function userDetails(name, email){
+//     console.log('My Name is ' + ": " + name + " " + "My Email is : " + email);
+// }
+// userDetails("Sudhir", "sudhir@gmail.com");
+
+
+const user = ()=>{
+    console.log("I am user");
+}
+
+user();
