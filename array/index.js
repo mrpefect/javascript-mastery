abaf76e3ca520bcbc77b165031cd6c84b7b9234e
sudhir kumar <sudhirkumar@sudhirs-MacBook-Air.local>
2026-02-01 08:52:37 +0530

    array

---
+++ array/index.js
@@ -0,0 +1,36 @@
+// array push method -> add element in array
+
+// let fruits = ["apple", "mango", "banana"];
+// console.log(fruits);
+// fruits.push("Pineapple");
+// console.log(fruits);
+
+// array pop method-> remove last element from array
+// let fruits = ["apple", "mango", "banana"];
+// console.log(fruits);
+// fruits.pop(fruits);
+// console.log(fruits)
+
+
+// array shift method-> remove first element from array
+// let fruits = ["apple", "mango", "banana"];
+// console.log(fruits)
+// fruits.shift(fruits)
+// console.log(fruits)
+
+
+// forEarch()
+
+let num = [1,2,3,4,5];
+
+num.forEach((item, index)=>{
+    console.log(`Item : ${item}, index : ${index}`);
+})
+
+
+
+// map()
+// filter()
+// find()
+
+
